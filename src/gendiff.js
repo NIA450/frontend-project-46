8396319dac0a9057e6205babdc525f01dc090307
Evgeniya Troshina <troshina450@gmail.com>
2024-11-12 17:17:22 +0300

    gitignore file added

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,27 +2,52 @@ import parseData from "./parser.js";
 import _ from 'lodash';
 const { sortBy } = _;
 
-function gendiff(filepath1, filepath2) {
+// function gendiff(filepath1, filepath2) {
+//   const data1 = parseData(filepath1);
+//   const data2 = parseData(filepath2);
+
+//   const keys = sortBy([...Object.keys(data1), ...Object.keys(data2)]);
+
+//   const result = keys.map((key) => {
+//     if (!(key in data1)) {
+//       return `  + ${key}: ${data2[key]}`; // Существуют только во втором файле
+//     } else if (!(key in data2)) {
+//       return `  - ${key}: ${data1[key]}`; // Существуют только в первом файле
+//     } else if (data1[key] !== data2[key]) {
+//       return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`; // Различия
+//     }
+//     return `    ${key}: ${data1[key]}`; // Значения совпадают
+//   });
+
+//   return `{\n${result.join("\n")}\n}`;
+
+//   //    console.log(`Comparing ${filepath1} and ${filepath2} with format: ${format}`);
+//   //    console.log('Data from file 1:', data1);
+//   //    console.log('Data from file 2:', data2);
+// }
+// export default gendiff;
+
+const gendiff = (filepath1, filepath2) => {
   const data1 = parseData(filepath1);
   const data2 = parseData(filepath2);
-
-  const keys = sortBy([...Object.keys(data1), ...Object.keys(data2)]);
+  
+  const keys = sortBy([...Object.keys(data1), ...Object.keys(data2)]); // Уникальные и отсортированные ключи
 
   const result = keys.map((key) => {
-    if (!(key in data1)) {
-      return `  + ${key}: ${data2[key]}`; // Существуют только во втором файле
-    } else if (!(key in data2)) {
-      return `  - ${key}: ${data1[key]}`; // Существуют только в первом файле
-    } else if (data1[key] !== data2[key]) {
-      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`; // Различия
-    }
-    return `    ${key}: ${data1[key]}`; // Значения совпадают
+      if (!(key in data1)) {
+          return `  + ${key}: ${data2[key]}`; // Только во втором файле
+      } else if (!(key in data2)) {
+          return `  - ${key}: ${data1[key]}`; // Только в первом файле
+      } else if (data1[key] !== data2[key]) {
+          return [
+              `  - ${key}: ${data1[key]}`, // Различие из первого файла
+              `  + ${key}: ${data2[key]}`  // Различие из второго файла
+          ].join('\n');
+      }
+      return `    ${key}: ${data1[key]}`; // Значения совпадают
   });
 
-  return `{\n${result.join("\n")}\n}`;
+  return `{\n${result.join('\n')}\n}`; // Формируем итоговый вывод
+};
 
-  //    console.log(`Comparing ${filepath1} and ${filepath2} with format: ${format}`);
-  //    console.log('Data from file 1:', data1);
-  //    console.log('Data from file 2:', data2);
-}
 export default gendiff;
--- src/parser.js
+++ src/parser.js
@@ -4,7 +4,15 @@ import path from "path";
 const parseData = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath);
   const data = fs.readFileSync(absolutePath, "utf-8"); // Читаем данные
-  // Преобразование JSON в объект
-  return JSON.parse(data); // Возвращаем объект
+  const extname = path.extname(absolutePath); // Получаем расширение файла
+
+  // Проверяем расширение и парсим данные
+  switch (extname) {
+    case '.json':
+      return JSON.parse(data); // Парсим JSON
+    default:
+      throw new Error(`Unsupported file type: ${extname}`); // Сообщение об ошибке
+  }
 };
+
 export default parseData;
